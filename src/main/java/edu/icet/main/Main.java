4e988fe8833bef39262db540acceb89459c0d0dc
Shean-D <sheandivyan01@gmail.com>
2025-08-02 13:42:13 +0530

    Initial commit : Set up Trends project Structure

--- src/main/java/edu/icet/main/Main.java
+++ src/main/java/edu/icet/main/Main.java
@@ -1,7 +1,7 @@
-package edu.icet;
+package edu.icet.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Starter.launch(args);
     }
 }
---
+++ src/main/java/edu/icet/main/Starter.java
@@ -0,0 +1,18 @@
+package edu.icet.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
